399c8985370d12f9a3ee43381803c0fa6cb76d7d
Gian Marco Simonazzi <gianmarco.simonazzi@studenti.unipr.it>
2024-03-14 12:47:29 +0100

    Added result message after run

--- entry.sh
+++ entry.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 cd /Isolette/Code
+## TODO: Handle build path
 
 if [[ "$1" == "run" ]]
 then
 	cd Test
-	./run_all_ut.sh && ./run_all_scenarios.sh
+	## TODO: Handle ut output (no return value)
+	(./run_all_ut.sh && echo -e '\e[1;32mUt ran successfully!\e[0m \U2705') || echo -e '\e[1;31mUt failed!\\e[0m \u274e\nExit code: $?'
+	(./run_all_scenarios.sh && echo -e '\e[1;32mScenarios ran successfully!\e[0m \U2705') || echo -e '\e[1;31mScenarios failed!\e[0m \u274e\nExit code: $?'
+	#./run_all_ut.sh && ./run_all_scenarios.sh
 else
 	./prepare.sh && ./build.sh
 	if [[ $? -eq 0 ]]
