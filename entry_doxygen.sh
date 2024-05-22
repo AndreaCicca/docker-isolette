3b410d22830371c20438a5cf39062078d5d4d847
Andrea Ciccarello <58073848+AndreaCicca@users.noreply.github.com>
2024-05-22 22:32:53 +0200

    update doxygen image and entry script

--- entry_doxygen.sh
+++ entry_doxygen.sh
@@ -2,21 +2,13 @@
 
 cd /Isolette/Code/
 
-rm -rf /Isolette/Code/@PATH_DOXYGEN_TEST_OUTPUT@/
-rm -rf /Isolette/Code/@PATH_DOXYGEN_SYSTEM_OUTPUT@/
-
-doxygen Doxyfile_test_2
-doxygen Doxyfile_system_2
-
 echo "Eseguo la compilazione doxygen"
 
-echo "Eseguo Compinazione Test"
+# Prepare.
+./prepare.sh
 
-cd /Isolette/Code/@PATH_DOXYGEN_TEST_OUTPUT@/latex
-make
-echo "Eseguita la compilzione Test"
+# Build.
+./build.sh
 
-echo "Eseguo Compinazioe System"
-cd /Isolette/Code/@PATH_DOXYGEN_SYSTEM_OUTPUT@/latex
-make
-echo "Eseguita la compilzione System"
+#Compile doxygen
+./compile_doxygen.sh
